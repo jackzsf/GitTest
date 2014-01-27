b39606882565b59fec2a4a917e0daceeeb95d572
jackzsf <zhangsaifeng1218@163.com>
2014-01-27 17:29:16 +0800

    eric github project first commit

---
+++ Eric_GitTest/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Eric_GitTest
+//
+//  Created by Eric on 14-1-27.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/*
+ guanxi_iMac (5f:07:6d:03:95:84:4b:37:0e:58:1d:6f:55:dc:75:cd)
+ */
+/*
+  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtLKirZ31xlQSrcLAF7Owo5PPWWF9aQQWW52T93i+0gTzMZPzzC9Vjg7OFYzmi3xQTpnOeKvKpg1P6X1OlovMT0AaJJaApnRe5wj/ZUkxioY4QURrIo4PvklsOAeQTHRq9G9ROrjRT8rB0F+GYEdFv/ytJlB3QfjSBW6KMhN66iPRQ2Gi5SOcP+mGVwZws9+7ABqiAiYbsBQOO2n0mROly/HErTviUaJACPNQGoZzsNcsXz/cpm01LVdCQHrr0ZIcXwZNfpGAqmxUe9itISnGSBQ2eutcIzoIzIHUdCgrVLGw3nR9KOXBynmZ2SIEVwIBSOuCjFHoZGW3Ll4D6yFSL ericzsf@guanxi.me
+ */
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Eric_GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Eric_GitTest
+//
+//  Created by Eric on 14-1-27.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
